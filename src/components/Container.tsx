79d06c9570b3915c6d48041f6ba319fd79694417
Andrew <carlandrewlinao@gmail.com>
2023-04-06 11:04:23 +0800

    first commit, add header

---
+++ src/components/Container.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./Header";
+import WordInformation from "./WordInformation";
+
+const Container = () => {
+	return (
+		<div className="max-w-md">
+			<Header />
+			<WordInformation />
+		</div>
+	);
+};
+
+export default Container;
