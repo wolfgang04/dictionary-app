ad13711aa9200a6e424fd538ef1bb44c1e57e1ba
Andrew <carlandrewlinao@gmail.com>
2023-04-29 08:58:26 +0800

    fix different viewports

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import React from "react";
 import Container from "./components/Container";
 
 function App() {
 	return (
-		<div className="p-5">
-			<Container />
+		<div className="flex w-screen h-screen justify-center">
+			<div className="py-5 md:py-10 px-[5%] min-w-[390px]">
+				<Container />
+			</div>
 		</div>
 	);
 }
